53e5bcdf5abaf0d03d65c0b1ca7e814c35eb8c3f
andynguyen20 <andynguyenn20@gmail.com>
2025-07-07 23:58:07 -0400

    Implemented multiplication script successfully

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,51 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+// R2 = R1 * R0
+
+//PSEUDOCODE 
+//n == R1
+//x == R0
+//sum = R2
+//for(i = 0; i < n, i++) {
+//    sum = sum + x;
+// }
+
+@i
+M=0 //store index starting point
+
+@R1
+D=M
+@n 
+M=D 
+
+@R2 
+M=0 //Initialize R2 so there is no stored input value
+
+@LOOP
+(LOOP)
+
+    @i
+    D=M 
+    @n 
+    D=D-M
+    @END
+    D;JEQ //Check if i == n and jump to end if it does
+
+
+    @R0 
+    D=M //Store RAM[0] value into D-register
+    @R2 
+    M=M+D //Add RAM[0] value into R2 
+    @i
+    M=M+1 //Increment the count for the loop
+    @LOOP //Jump back to loop ROM to re-iterate
+    0;JMP
+
+(END)
+    @END
+    0;JMP //End loop (best practice)
+
+
+
