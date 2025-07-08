7d4c769fcdd92a1bbba15155f6ac3033150f675a
andynguyen20 <andynguyenn20@gmail.com>
2025-07-08 15:09:02 -0400

    Implemented  fill script

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -9,3 +9,74 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+
+@LOOP
+(LOOP)
+    @KBD 
+    D=M //Store keyboard input value into D-Register (noInput = 0, input !=0)
+
+    @DRAW 
+    D; JNE //Jump to drawing ROM address if key is pressed
+
+    @CLEAR
+    D; JEQ //Jump to clearing ROM address if no key is pressed
+
+    (DRAW)
+        @SCREEN 
+        D=A 
+        @i 
+        M=D //initialize the incrementor i to the first address of screen everytime
+
+    (DRAW_LOOP)
+        @i 
+        D=M 
+        @24576 //last address of screen + 1
+        D=D-A 
+        @END_DRAW 
+        D;JEQ //if exits the last address of the screen then go to END_DRAW
+
+        @i 
+        A=M
+        M=-1 //draw black (-1 = 1111111111111111) onto current address 
+
+
+        @i 
+        M=M+1 //increment the count 
+        @DRAW_LOOP
+        0; JMP //loop back
+
+    (END_DRAW) 
+        @LOOP 
+        0; JMP //go back to start of loop and reevaluate key input 
+    
+    (CLEAR)
+        @SCREEN 
+        D=A 
+        @i //initialize incrementor to first address
+        M=D
+
+    (CLEAR_LOOP)
+        @i 
+        D=M 
+        @24576
+        D=D-A
+        @END_CLEAR
+        D;JEQ //if exits last address of screen then go to END_CLEAR 
+
+        @i 
+        A=M
+        M=0 //clears the screen to white (0 = 0000000000000000) on current address
+
+        @i 
+        M=M+1 //increment the address count
+        @CLEAR_LOOP
+        0; JMP //loop back
+
+    (END_CLEAR)
+        @LOOP 
+        0; JMP //go back to start of loop and reevalute key input
+
+
+
+
